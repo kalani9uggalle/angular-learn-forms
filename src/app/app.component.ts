865a952b228b71812314c357b709d565ab09a9a1
kalani9uggalle <kalani9uggalle@gmail.com>
2022-01-27 13:55:29 +0530

    form control-form group

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Forms_proj';
+  registration :FormGroup =new FormGroup({
+    name1:new FormControl('Name',Validators.required),
+    name2:new FormControl('',Validators.required),
+    agree:new FormControl(false),
+    address:new FormGroup({postal_code: new FormControl(''),
+      state:new FormControl(''),
+      city:new FormControl('')
+    })
+  })
+
+  load_api_data(){
+    this.registration.patchValue({
+      name1:'Kalani',
+    name2:'Prabodha',
+    agree:false,
+   
+
+  })
+
 }
+
+}
+ 
